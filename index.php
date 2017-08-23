c64d6d23d3c64e714e31484289aaf4b6dd73c13d
Alex <flidde@googlemail.com>
2017-08-23 11:17:52 -0400

    navbar

--- index.php
+++ index.php
@@ -13,24 +13,12 @@
 </head>
 <body>
 <nav>
-
-    <ul id="slide-out" class="side-nav">
-        <li><div class="user-view">
-                <div class="background">
-                    <img src="images/office.jpg">
-                </div>
-                <a href="#!user"><img class="circle" src="images/yuna.jpg"></a>
-                <a href="#!name"><span class="white-text name">John Doe</span></a>
-                <a href="#!email"><span class="white-text email">jdandturk@gmail.com</span></a>
-            </div></li>
-        <li><a href="#!"><i class="material-icons">cloud</i>First Link With Icon</a></li>
-        <li><a href="#!">Second Link</a></li>
-        <li><div class="divider"></div></li>
-        <li><a class="subheader">Subheader</a></li>
-        <li><a class="waves-effect" href="#!">Third Link With Waves</a></li>
-    </ul>
-    <a href="#" data-activates="slide-out" class="button-collapse"><i class="material-icons">menu</i></a>
-
+    <div class="nav-wrapper">
+        <a href="index.php" class="brand-logo">Logo</a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><a href="Fuhrpark.html">Fuhrpark</a></li>
+        </ul>
+    </div>
 </nav>
 	<div class="container">
 		<h2>Fuhrparkmanagement</h2>
