90f1b159f9ba4777be28550ff412b3ef703c40e4
Alex <flidde@googlemail.com>
2017-10-13 15:28:41 +0200

    New index page with Image and information text

--- index.php
+++ index.php
@@ -24,10 +24,14 @@
         </div>
         <div class="card-stacked">
             <div class="card-content">
-                <p>I am a very simple card. I am good at containing small bits of information.</p>
+                <p>Hallo liebe Mitarbeiter,<br><br>
+                    Unser Ökosystem ist immer stärker werdenden Belastungen in unterschiedlichsten Formen ausgesetzt und Umweltschutz findet auf ebenso vielen Ebenen statt und ist in vielfältiger Weise mit wirtschaftlichem Handeln verknüpft. Wir als zukunftsorientiertes Unternehmen stehen damit auch in der Pflicht unseren Teil für eine gesündere Umwelt beizutragen. Wir müssen uns Lösungen überlegen wie wir zukunftsorientierter handeln einen positiven Beitrag zur Nachhaltig leisten können. Hier kommt die Verbindung zu dem neu eingeführten Fuhrparkmanagementsystem. Es wurde in vielen Forschungen festgestellt, dass durch den Einsatz von Informationssystemen sich Unternehmen in vielen Bereichen ökologischer und nachhaltiger aufstellen können. Somit möchten wir durch ein so genanntes Green IS einen ersten Schritt auf diesem Gebiet machen genauer gesagt im Bereich der Green IS Transportation. Was ist das Green IS Transportation?<br><br>
+                    Um Green IS Transportation spezifiziert darzustellen, wollen wir zuerst die Green Transportation genauer beleuchten. Unter Green Transportation versteht sich jede Art von Beförderung von Gegenständen oder Personen durch Fahrzeuge die keinen negativen Einfluss auf die nähere Umgebung und Umwelt haben. Und hier kommt der Übergang zur Green IS Transportation, es geht also darum durch eine Software, ein Flottenmanagement zu ermöglichen das die effektive und der sinnhaften Nutzung von Ressourcen in Form von Fahrzeugen zum Personentransport in Unternehmen ermöglicht. Es soll durch ein IS ein umweltfreundlicheres und nachhaltigeres Flottenmanagement im Bereich der Dienstwagenvergabe ermöglicht werden.<br><br>
+                    Wir sind fest davon überzeugt dass wir mit der Einführung dieser Software einen richtigen Schritt in die Richtung machen.<br><br><br><br>
+                </p>
             </div>
             <div class="card-action">
-                <a href="#">This is a link</a>
+                <a href="https://www.conserve-energy-future.com/modes-and-benefits-of-green-transportation.php">Für mehr Informationen klicken Sie hier!</a>
             </div>
         </div>
     </div>
