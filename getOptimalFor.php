5adaf30e37228d7c36599acf9d3e8f22b928f1de
Alex <flidde@googlemail.com>
2017-10-13 15:00:22 +0200

    New index page with Image and information text

--- getOptimalFor.php
+++ getOptimalFor.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Alex
- * Date: 10.10.2017
- * Time: 14:42
- */
 
 /**Leitet die Anfragen weiter an die API.php // Wayback gibt an, ob der Rückweg mit einbezogen werden soll*/
 $wayback = isset($_GET["wayback"]);
--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Fuhrpark</title>
+	<title>Fuhrparkmanagement</title>
 
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
@@ -11,10 +11,28 @@
 <body>
 <?php include "fragments/navbar.html"; ?>
 	<div class="container">
-		<h3>Hausarbeitsseminar</h3>
-		<h5>Willkommen auf der Fuhrparkmanagement Software</h5>
-		<p>Diese Software soll die Ökologische bla bla</p>
+		<h3>Fuhrparkmanagement</h3>
+		<h5>In der Fuhrparkmanagement-Software sehen Sie, welcher Dienstwagen oder Bahnverbindung Ihnen für Ihre Dienstreise zur Verfügung steht. </h5>
 	</div>
+    <br>
+<div class="container">
+<div class="col s12 m7">
+    <h3 class="header">Green IS</h3>
+    <div class="card horizontal">
+        <div class="card-image">
+            <img src="img/greenIS.jpg">
+        </div>
+        <div class="card-stacked">
+            <div class="card-content">
+                <p>I am a very simple card. I am good at containing small bits of information.</p>
+            </div>
+            <div class="card-action">
+                <a href="#">This is a link</a>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
 <?php include "fragments/footer.html"; ?>
 </body>
 </html>
